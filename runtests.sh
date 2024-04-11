7e43dfdda4142848ccb3ffba976ac48809f23bcf
Ross Voerman <voermanr@oregonstate.edu>
2024-04-11 13:27:49 -0700

    Added some basic curl testing.

---
+++ runtests.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+SURL=http://localhost:6969
+
+status() {
+    printf "\n=====================================================\n"
+    printf "%s\n" "$1"
+    printf -- "-----------------------------------------------------\n"
+}
+
+status 'POST a new business'
+curl -X POST \
+    -H 'Content-Type: application/json' \
+    -d '{"name":"Bobs Burgers", "city":"Bobtown", "state":"PN", "zip":"12700", "street_address":"420", "phone_number":"867-5309", "category":"burgerstand", "subcategory":"with_fries"}' \
+    $SURL/businesses/
+echo ""
+
+status 'fail to POST a new business without name or zip'
+curl -X POST \
+    -H 'Content-Type: application/json' \
+    -d '{"city":"Bobtown", "state":"PN", "street_address":"420", "phone_number":"867-5309", "category":"burgerstand", "subcategory":"with_fries"}' \
+    $SURL/businesses/
+echo ""
+
+status 'GET a business'
+curl $SURL/businesses/0
+echo ""
+
+status 'GET every business'
+
+# etc.
