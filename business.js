6a87254020104eeede92bdd923aeb117e803f484
Ross Voerman <voermanr@oregonstate.edu>
2024-04-11 13:27:25 -0700

    Added Business class.

---
+++ business.js
@@ -0,0 +1,14 @@
+class Business {
+    constructor({name, streetAddress, city, state, zipCode, phoneNumber, category, subcategory, website, email}) {
+        this.name = name;
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+        this.category = category;
+        this.subcategory = subcategory;
+        this.website = website;
+        this.email = email;
+    }
+}
