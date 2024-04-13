6466e79139b77b43b78a3879e0f641e74300f3d4
Ross Voerman <voermanr@oregonstate.edu>
2024-04-13 16:37:47 -0700

    Fixing up reviews

--- review.js
+++ review.js
@@ -1,4 +1,18 @@
 class Review {
-    constructor() {
+    constructor(id, {starRating, dollarSignRating, reviewBody}) {
+        this.starRating = starRating;
+        this.dollarSignRating = dollarSignRating;
+        this.reviewBody = reviewBody;
+        this.links = [{"self":`/reviews/${id}`}]
+    }
+
+    updateFields(desiredFields) {
+        for (let field in desiredFields) {
+            if (field in this) {
+                this[field] = desiredFields[field];
+            }
+        }
     }
 }
+
+module.exports = { Review };
