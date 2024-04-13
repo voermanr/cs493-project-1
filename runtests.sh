a0eea5fe627d82351e84936547b560a8a6294e7f
Ross Voerman <voermanr@oregonstate.edu>
2024-04-13 08:34:50 -0700

    Making tests better

--- runtests.sh
+++ runtests.sh
@@ -6,33 +6,45 @@ status() {
     printf "\n=====================================================\n"
     printf "%s\n" "$1"
     printf -- "-----------------------------------------------------\n"
+    printf "%s\n" "$2"
+    printf "=====================================================\n"
 }
 
-status 'POST a new business'
-curl -X POST \
+# POST a new business
+output=$(curl -X POST -s \
     -H 'Content-Type: application/json' \
     -d '{"name":"Bobs Burgers", "city":"Bobtown", "state":"PN", "zip":"12700", "street_address":"420", "phone_number":"867-5309", "category":"burgerstand", "subcategory":"with_fries"}' \
-    $SURL/businesses/
-echo ""
+    $SURL/businesses/)
+status 'POST a new business' "$output"
 
-status 'fail to POST a new business without name or zip'
-curl -X POST \
+# fail to post a business
+output=$(curl -X POST -s \
     -H 'Content-Type: application/json' \
     -d '{"city":"Bobtown", "state":"PN", "street_address":"420", "phone_number":"867-5309", "category":"burgerstand", "subcategory":"with_fries"}' \
-    $SURL/businesses/
-echo ""
+    $SURL/businesses/)
+status 'fail to POST a new business without name or zip' "${output}"
 
-status 'GET a business'
-curl $SURL/businesses/0
-echo ""
+# GET a business
+output=$(curl -s $SURL/businesses/0)
+status 'GET a business' "${output}"
 
-status 'GET every business'
-curl $SURL/businesses/
-echo ""
+# PATCH a business
+curl -X PATCH -s \
+  -H 'Content-Type: application/json' \
+  -d '{"name":"Jimmy Pestos Insurrection Pizza"}' \
+  $SURL/businesses/0 > /dev/null
+output=$(curl -s $SURL/businesses/0)
+status 'PATCH name on a business' "${output}"
 
-status 'PATCH name on a business'
-curl -X PATCH \
-  -d '{"name":"Jimmy Pestos Insurrection Pizza}' \
-  $SURL/businesses/0
+curl -X POST -s \
+    -H 'Content-Type: application/json' \
+    -d '{"name":"A Deletable Place", "city":"Bobtown", "state":"PN", "zip":"12700", "street_address":"420", "phone_number":"867-5309", "category":"burgerstand", "subcategory":"with_fries"}' \
+    $SURL/businesses/ > /dev/null
+curl -X DELETE -s\
+  $SURL/businesses/1 > /dev/null
+output=$(curl -s $SURL/businesses/1)
+status 'DELETE a business' "${output}"
 
+output=$(curl -s $SURL/businesses/)
+status 'GET every business' "${output}"
 # etc.
