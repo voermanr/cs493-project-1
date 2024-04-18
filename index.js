23ad768461c63ee9b1808f6166b760fe8c01f04f
Ross Voerman <voermanr@oregonstate.edu>
2024-04-18 11:53:20 -0700

    Moving route handling to controllers

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Project 1 Check-in 2
 
 let express = require('express');
-const {Business} = require("./business");
-const {Review} = require("./review");
-const {Photo} = require("./photo");
+const {Review} = require("./models/review");
+const {Photo} = require("./models/photo");
+
+const busCon = require("./controllers/businessesController");
+
+const {findMissingParams} = require("./middeware/findMissingParameters");
+const {updateBusinessById} = require("./controllers/businessesController");
+
 let app = express();
 
 let businesses = [];
@@ -23,118 +28,22 @@ app.listen(port, () => {
     console.log(`Listening on ${port}...`)
 })
 
-function findMissingParams(req, res, next) {
-    let missingParams = [];
-    let requiredParams = req.requiredParams;
-
-    requiredParams.forEach(param => {
-        if (!req.body.hasOwnProperty(param)) {
-            missingParams.push(`${param}`);
-        }
-    });
-
-    if (missingParams.length > 0) {
-        res.status(400).send("missing parameters: " + missingParams.join(", "))
-    }
-
-    next()
-}
-
-function prepareBusinessParams(req, res, next) {
-    req.requiredParams = ['name', 'street_address', 'city', 'state', 'zip', 'phone_number', 'category', 'subcategory'];
-
-    next();
-}
-
-function createBusiness(req, res) {
-    let id = businesses_counter;
-    businesses[id] = new Business(id, req.body);
-    businesses_counter++;
-    res.status(201).json({
-        'id': id,
-        'links': [
-            {'self': `/businesses/${id}`}
-        ]
-    });
-}
-
 // User adds a new business
-app.post('/businesses/', prepareBusinessParams, findMissingParams, createBusiness)
-
-
-// User updates a business
-app.patch('/businesses/:id', (req, res) => {
-    //TODO: verify that the update contains only patchable fields
-
-    let id = req.params.id;
-
-    if (id in businesses) {
-        businesses[id].updateFields(req.body);
-
-        res.status(200).send({
-            'links': [
-                {'self': `/businesses/${id}`}
-            ]
-        });
-    }
-    else
-        res.sendStatus(404);
-})
-
-// User removes a business
-app.delete('/businesses/:id', (req, res) => {
-    let id = req.params.id;
-
-    if (id in businesses) {
-        delete businesses[id];
-        res.sendStatus(204);
-    }
-    else
-        res.status(404).send(`Business not found.`);
-});
-
-// User gets a specific business
-app.get('/businesses/:id', (req, res) => {
-    let id = req.params.id;
-
-    if (id in businesses) {
-        res.status(200).send(businesses[id]);
-    }
-    else
-        res.status(404).send('Business not found.');
-})
+app.post('/businesses/', busCon.setRequiredBusinessParams, findMissingParams, busCon.createNewBusiness);
 
 // User gets a list of all businesses
-app.get('/businesses/', (req, res) => {
-    let page = parseInt(req.query.page) || 1;
-    let numPerPage = 10;
-    let lastPage = Math.ceil(businesses.length /numPerPage);
-    page = page < 1 ? 1 : page;
-    page = page > lastPage ? lastPage : page;
+app.get('/businesses/', busCon.getAllBusinesses);
 
-    let start = (page - 1) * numPerPage;
-    let end = start + numPerPage;
-    let pageBusinesses = businesses.slice(start, end);
+// User gets a specific business
+app.get('/businesses/:id', busCon.getBusinessById);
 
-    let links = {};
-    if (page < lastPage) {
-        links.nextPage = '/businesses?page=' + (page + 1);
-        links.lastPage = '/businesses?page=' + lastPage;
-    }
-    if (page > 1) {
-        links.prevPage = '/businesses?page=' + (page - 1);
-        links.firstPage = '/businesses?page=1';
-    }
+// User updates a business
+app.patch('/businesses/:id', busCon.updateBusinessById);
 
-    res.status(200).json({
-        "pageNumber": page,
-        "totalPages": lastPage,
-        "pageSize": numPerPage,
-        "totalCount": businesses.length,
-        "businesses": pageBusinesses,
-        "links": links
-    });
-})
+// User removes a business
+app.delete('/businesses/:id', busCon.deleteBusinessById);
+
+
 
 // User posts a new review
 app.post('/reviews/', (req, res) => {
